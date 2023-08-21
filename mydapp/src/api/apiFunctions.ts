2477a5ae80e3d4e5634ad681a11f7299404140d7
Lukway <lukway.dev@gmail.com>
2023-08-21 20:40:22 -0300

    [Fix] API Functions

--- mydapp/src/api/apiFunctions.ts
+++ mydapp/src/api/apiFunctions.ts
@@ -1,139 +1,156 @@
-import axios from 'axios'
-import { API_URL } from '../constants'
+import axios, { AxiosError } from 'axios';
+
+import { API_URL } from '../constants';
 
 interface GetQuery {
-    path: string;
-    token?: string;
-    params?: any;
-    callback?: (data: any) => any;
+  path: string;
+  token?: string;
+  params?: any;
+  callback?: (data: any) => any;
 }
 
 interface GeneralQuery {
-    path: string;
-    token?: string;
-    body?: any;
-    callback?: (data: any) => any;
+  path: string;
+  token?: string;
+  body?: any;
+  callback?: (data: any) => any;
 }
 
 export const getQuery = async <T>({
   path,
   token = '',
   params = {},
-  callback = (data) => data,
+  callback = data => data,
 }: GetQuery): Promise<T> => {
-  const headers = token && {
-    Authorization: `Token ${token}`,
+  const headers: Record<string, string> = {}; // Define los headers como un objeto vacío
+
+  if (token) {
+    headers.Authorization = `Token ${token}`;
   }
 
   try {
     const { data } = await axios.get(`${API_URL}${path}`, {
       headers,
       params,
-    })
+    });
 
-    return callback(data)
+    return callback(data);
   } catch (error) {
-    return handleError(error)
+    return handleError(error);
   }
-}
+};
 
 export const postQuery = async <T>({
   path,
   token = '',
   body = {},
-  callback = (data) => data,
+  callback = data => data,
 }: GeneralQuery): Promise<T> => {
-  const headers = token && {
-    Authorization: `Token ${token}`,
+  const headers: Record<string, string> = {}; // Define los headers como un objeto vacío
+
+  if (token) {
+    headers.Authorization = `Token ${token}`;
   }
 
   try {
     const { data } = await axios.post(`${API_URL}${path}`, body, {
       headers,
-    })
+    });
 
-    return callback(data)
+    return callback(data);
   } catch (error) {
-    return handleError(error)
+    return handleError(error);
   }
-}
+};
 
 export const deleteQuery = async <T>({
   path,
   token = '',
   body = {},
-  callback = (data) => data,
+  callback = data => data,
 }: GeneralQuery): Promise<T> => {
-  const headers = token && {
-    Authorization: `Token ${token}`,
+  const headers: Record<string, string> = {}; // Define los headers como un objeto vacío
+
+  if (token) {
+    headers.Authorization = `Token ${token}`;
   }
 
   try {
     const { data } = await axios.delete(`${API_URL}${path}`, {
       headers,
       data: body,
-    })
+    });
 
-    return callback(data)
+    return callback(data);
   } catch (error) {
-    return handleError(error)
+    return handleError(error);
   }
-}
+};
 
 export const putQuery = async <T>({
   path,
   token = '',
   body = {},
-  callback = (data) => data,
+  callback = data => data,
 }: GeneralQuery): Promise<T> => {
-  const headers = token && {
-    Authorization: `Token ${token}`,
+  const headers: Record<string, string> = {}; // Define los headers como un objeto vacío
+
+  if (token) {
+    headers.Authorization = `Token ${token}`;
   }
 
   try {
     const { data } = await axios.put(`${API_URL}${path}`, body, {
       headers,
-    })
+    });
 
-    return callback(data)
+    return callback(data);
   } catch (error) {
-    return handleError(error)
+    return handleError(error);
   }
-}
+};
 
 export const patchQuery = async <T>({
   path,
   token = '',
   body = {},
-  callback = (data) => data,
+  callback = data => data,
 }: GeneralQuery): Promise<T> => {
-  const headers = token && {
-    Authorization: `Token ${token}`,
-    // 'Content-Type': 'multipart/form-data',
+  // const headers = token && {
+  //   Authorization: `Token ${token}`,
+  //   // 'Content-Type': 'multipart/form-data',
+  // };
+  const headers: Record<string, string> = {}; // Define los headers como un objeto vacío
+
+  if (token) {
+    headers.Authorization = `Token ${token}`;
   }
 
   try {
     const { data } = await axios.patch(`${API_URL}${path}`, body, {
       headers,
-    })
+    });
 
-    return callback(data)
+    return callback(data);
   } catch (error) {
-    return handleError(error)
+    return handleError(error);
   }
-}
+};
 
 const deleteInvalidToken = () => {
-  localStorage.removeItem('token')
-  window.dispatchEvent(new Event('storage'))
-}
+  localStorage.removeItem('token');
+  window.dispatchEvent(new Event('storage'));
+};
 
-const handleError = (error) => {
+const handleError = (error: any) => {
   if (
-    typeof error.response.data === 'string' &&
+    typeof error.response?.data === 'string' &&
     error.response.data.startsWith('<!DOCTYPE html>')
-  )
-    return { error: 'internal server error' }
-  if (error.response.data.detail == 'Invalid token.') deleteInvalidToken()
-  return error.response.data
-}
+  ) {
+    return { error: 'internal server error' };
+  }
+  if (error.response?.data.detail === 'Invalid token.') {
+    deleteInvalidToken();
+  }
+  return error.response?.data;
+};
