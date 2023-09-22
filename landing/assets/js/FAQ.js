a94ee5570d2d5200ac598b2038dbb1ee44c72757
Juan Borrelli <nacho2911@hotmail.com>
2023-09-21 21:57:48 -0300

    landing

--- landing/assets/js/FAQ.js
+++ landing/assets/js/FAQ.js
@@ -1,11 +1,26 @@
 document.querySelectorAll('.faq-question').forEach(question => {
-    question.addEventListener('click', () => {
-      const answer = question.nextElementSibling;
-      document.querySelectorAll('.faq-answer').forEach(a => {
-        if (a !== answer) {
-          a.classList.remove('show');
-        }
-      });
-      answer.classList.toggle('show');
+  question.addEventListener('click', () => {
+    const answer = question.nextElementSibling;
+    document.querySelectorAll('.faq-answer').forEach(a => {
+      if (a !== answer) {
+        a.classList.remove('show');
+      }
     });
+    answer.classList.toggle('show');
   });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const faqItems = document.querySelectorAll('.faq-item');
+
+  faqItems.forEach(function(item) {
+      const question = item.querySelector('.faq-question');
+      const answer = item.querySelector('.faq-answer');
+      const toggleIcon = question.querySelector('.toggle-icon');
+
+      question.addEventListener('click', function() {
+          answer.classList.toggle('visible');
+          toggleIcon.textContent = toggleIcon.textContent === '▼' ? '▲' : '▼';
+      });
+  });
+});
